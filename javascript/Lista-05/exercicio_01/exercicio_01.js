6eff730f6c629ce91f8e8d96b33a5d17ff0e9996
Antônio Netto <114105731+nettodmlo@users.noreply.github.com>
2023-10-05 23:40:16 -0300

    Update exercicio_01.js

--- javascript/Lista-05/exercicio_01/exercicio_01.js
+++ javascript/Lista-05/exercicio_01/exercicio_01.js
@@ -4,3 +4,16 @@
 // calcule e escreva o número de anos necessários para que a população do país A 
 // ultrapasse ou iguale a população do país B, mantidas as taxas de crescimento.
 
+var populacaoA = 80000;
+var taxaDeCrescimentoA = 0.03;
+var populacaoB = 200000;
+var taxaDeCrescimentoB = 0.015;
+var anosDecorridos = 0;
+
+while (populacaoA < populacaoB) {
+  populacaoA += populacaoA * taxaDeCrescimentoA;
+  populacaoB += populacaoB * taxaDeCrescimentoB;
+  anosDecorridos++;
+}
+
+console.log("Levará " + anosDecorridos + " anos para que a população do país A ultrapasse ou iguale a população do país B.");
